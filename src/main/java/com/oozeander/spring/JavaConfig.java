7833281a9e5d400d88bd29068bedaa7188f0d9c6
Oozeander <billel.ketrouci@gmail.com>
2020-12-16 16:22:41 +0100

    Swagger API Documentation

--- src/main/java/com/oozeander/spring/JavaConfig.java
+++ src/main/java/com/oozeander/spring/JavaConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({ PersistenceConfig.class, RestConfig.class })
+@Import({ PersistenceConfig.class, RestConfig.class, SwaggerConfig.class })
 public class JavaConfig {
+
 }
--- src/main/java/com/oozeander/spring/RestConfig.java
+++ src/main/java/com/oozeander/spring/RestConfig.java
@@ -3,9 +3,17 @@ package com.oozeander.spring;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "com.oozeander.resource", "com.oozeander.service" })
-public class RestConfig {
+public class RestConfig extends WebMvcConfigurerAdapter {
+	// Optional for Spring Boot
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
 }
